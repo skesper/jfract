028844c53b7eea1dc2a27f39b72d90732a4d802b
skesper <stephan.kesper@gmail.com>
2013-03-02 19:55:59 +0100

    another fractal

--- src/main/java/de/jfract/math/Chaos2.java
+++ src/main/java/de/jfract/math/Chaos2.java
@@ -8,14 +8,34 @@ public class Chaos2 extends Fractal {
 
 	@Override
 	protected Complex calc(Complex z, Complex c) {
-		Complex zz = z.square().add(one);
-		return zz.divide(z.add(c));
+        double zreal, zimag, creal, cimag;
+        double areal, aimag, breal, bimag;
+        double n, c1real, c1imag;
+
+        creal = c.real();
+        cimag = c.imaginary();
+
+        zreal = z.real();
+        zimag = z.imaginary();
+
+
+        areal = zreal * zreal - zimag * zimag + creal;
+        aimag = 3. * zreal + creal;
+        breal = 2. * zreal * zimag + cimag;
+        bimag = 3. * zimag + cimag;
+
+        n = aimag * aimag + bimag * bimag;
+
+        c1real = (areal * aimag+breal * bimag) / n;
+        c1imag = (aimag * breal-areal * bimag) / n;
+
+        return new Complex(c1real * c1real - c1imag * c1imag,2. * c1real * c1imag);
 	}
 
 
     @Override
     public UsageType getType() {
-        return UsageType.NONE;
+        return UsageType.START_POINT;
     }
 
     @Override
@@ -25,7 +45,7 @@ public class Chaos2 extends Fractal {
 
     @Override
     public Complex getPreferredStartPoint() {
-        return null;
+        return new Complex(0.,0.);
     }
 
     @Override
